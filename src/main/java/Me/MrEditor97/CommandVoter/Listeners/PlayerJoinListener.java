3e8fdb330a93be7ede3860e0328ac2253753e3bd
Sam Hughes <Sam@empirics.plus.com>
2014-08-11 17:46:07 +0100

    Update PlayerJoinListener.java

---
+++ src/main/java/Me/MrEditor97/CommandVoter/Listeners/PlayerJoinListener.java
@@ -0,0 +1,53 @@
+package Me.MrEditor97.CommandVoter.Listeners;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import Me.MrEditor97.CommandVoter.CommandVoter;
+import Me.MrEditor97.CommandVoter.Messages.Message;
+import Me.MrEditor97.CommandVoter.Util.Util;
+
+public class PlayerJoinListener implements Listener {
+	
+	CommandVoter plugin;
+	
+	public PlayerJoinListener(CommandVoter instance) {
+		plugin = instance;
+	}
+	
+	//On Player Join Event
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		Message message = new Message(plugin);
+		Util util = new Util(plugin);
+		
+		Player player = event.getPlayer();
+		
+		//Check whether player already exists in the configuration file
+		if(util.nodeExists(plugin.getConfig(), "players." + player.getName().toLowerCase()) == true) {
+			String joinMessage = plugin.getConfig().getString("playerJoinMessage");
+			int timesVoted = plugin.getConfig().getInt("players." + player.getName().toLowerCase() + ".voted");
+			
+			//Display message
+			message.join(player, joinMessage, timesVoted);
+		} else {
+			//Add player attributed to the configuration file
+			plugin.getConfig().set("players." + player.getName().toLowerCase() + ".username", player.getName());
+			plugin.getConfig().set("players." + player.getName().toLowerCase() + ".uuid", player.getUniqueId().toString());
+			plugin.getConfig().set("players." + player.getName().toLowerCase() + ".voted", 0);
+			plugin.getConfig().set("players." + player.getName().toLowerCase() + ".rewarded", true);
+			
+			plugin.getConfig().options().copyDefaults(true);
+			plugin.saveConfig();
+			
+			String joinMessage = (String) plugin.getConfig().get("playerJoinMessage");
+			int timesVoted = (int) plugin.getConfig().get("players." + player.getName().toLowerCase() + ".voted");
+			
+			//Display message
+			message.join(player, joinMessage, timesVoted);
+		}
+	}	
+}
