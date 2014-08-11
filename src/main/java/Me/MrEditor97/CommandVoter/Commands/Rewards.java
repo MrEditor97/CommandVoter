f2609eec20735c22e43d0349864ae4b222c2a6ea
Sam Hughes <Sam@empirics.plus.com>
2014-08-11 17:53:43 +0100

    Update Rewards.java

---
+++ src/main/java/Me/MrEditor97/CommandVoter/Commands/Rewards.java
@@ -0,0 +1,46 @@
+package Me.MrEditor97.CommandVoter.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import Me.MrEditor97.CommandVoter.CommandVoter;
+import Me.MrEditor97.CommandVoter.Messages.Message;
+import Me.MrEditor97.CommandVoter.Rewards.Reward;
+
+public class Rewards {
+	
+	CommandVoter plugin;
+	
+	public Rewards(CommandVoter instance) {
+		plugin = instance; 
+	}
+	
+	//When the player peforms the command
+	@SuppressWarnings("deprecation")
+	public void command(boolean permission, CommandSender sender, Command cmd, String label, String[] args) {
+		Message message = new Message(plugin);
+		Reward reward = new Reward(plugin);
+		
+		Player player = (Player) sender;
+		
+		//If a player has only typed /rewards
+		if (args.length == 0) {
+			reward.get(player);
+		} else {
+			//Check if the player has permission to do /rewards [player]
+			if (permission = true) {
+				Player target = plugin.getServer().getPlayer(args[0]);
+				
+				if (target == null) {
+					message.online(player, plugin.getConfig().getString("messages.playerNotOnline"), args[0]);
+				} else {
+					reward.getOther(player, target);
+				}
+			} else if (permission = false) {
+				//Message player
+				message.permission(player, plugin.getConfig().getString("messages.permission"));
+			}
+		}
+	}
+}
